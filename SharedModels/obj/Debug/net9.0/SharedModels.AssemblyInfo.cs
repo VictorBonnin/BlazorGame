56b55cd7627a7fbdaf189cbfb69f37ff4aa9c48b
Victor-Bonnin <victor.bonnin@edu.devinci.fr>
2025-10-10 16:53:25 +0200

    Modifs HintBox

--- SharedModels/obj/Debug/net9.0/SharedModels.AssemblyInfo.cs
+++ SharedModels/obj/Debug/net9.0/SharedModels.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca726d2cb385abc7b74fa350a22f8eb7da8106a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14acf89a14379c6dbf4cff7430bc8de12b118b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
